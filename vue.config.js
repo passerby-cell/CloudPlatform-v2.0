5702775ca3b82fb563438f1dd8b07885c2d3589d
LawInBee <18606145168@163.com>
2023-09-21 20:25:01 +0800

    修改全局IP路径

--- vue.config.js
+++ vue.config.js
@@ -50,14 +50,21 @@ module.exports = defineConfig({
         },
       },
       "/satelite": {
-        target: "http://127.0.0.1:8087",
+        target: "http://10.33.50.5:81",
         secure: false,
         changeOrigin: true, // 代理时是否更改host
         // pathRewrite: {
         //   "^/satelite": "", //这里理解成用'/api'代替target里面的地址,比如我要调用'http://40.00.100.100:3002/user/add'，直接写'/api/user/add'即可
         // },
       },
-
+      // "/satelite": {
+      //   target: "http://127.0.0.1:8087",
+      //   secure: false,
+      //   changeOrigin: true, // 代理时是否更改host
+      //   // pathRewrite: {
+      //   //   "^/satelite": "", //这里理解成用'/api'代替target里面的地址,比如我要调用'http://40.00.100.100:3002/user/add'，直接写'/api/user/add'即可
+      //   // },
+      // },
       // "/remotesatelite": {
       //   target: "https://data.cresda.cn/",
       //   secure: false,
